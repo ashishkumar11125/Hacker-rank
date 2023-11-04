d832deb4eada63c7bb31328c5af05766f02845e1
Ashish Kumar <ashishkumar11125@hotmail.com>
2023-11-04 23:49:22 +0530

    Create Structs.cpp

---
+++ Structs.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student {
+    int age, standardOfStudent;
+    string first_name, last_name;
+};
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    Student s;
+    cin >> s.age  >> s.first_name >> s.last_name >> s.standardOfStudent;
+    cout << s.age  <<" "<< s.first_name <<" "<< s.last_name << " "<<s.standardOfStudent;
+    return 0;
+}
